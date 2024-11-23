38fd57c1e46d4f58112a616f03c92989d519666c
SyedMoin-Lab <syedmoinuddin106@gmail.com>
2024-11-23 16:37:52 +0530

    Sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,3 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import { projectsType } from "./project";
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [projectsType]

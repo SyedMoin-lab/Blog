3c293915e004293b3026d8cdcf8e7e9f19af64f3
SyedMoin-Lab <syedmoinuddin106@gmail.com>
2024-11-23 15:36:49 +0530

    I have added superbase and Prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import { Button } from "@/components/ui/button";
 
+import { AboutSection } from "./components/AboutSection";
+import { FavoriteProjects } from "./components/FavoriteProjects";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { SectionTwo } from "./components/SectionTwo";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hello</Button>Hello</div>
-  )
-
+    <div className="max-w-7xl w-full px-4 md:px-8 mx-auto">
+      <Hero />
+      <SectionTwo />
+      <AboutSection />
+      <FavoriteProjects />
+      <Footer />
+    </div>
+  );
 }
